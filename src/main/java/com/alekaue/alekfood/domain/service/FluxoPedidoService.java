7f75c5f6e28daf977de7d0143e956cc43663c71f
Alessandro Brito <alekaue.brito@gmail.com>
2020-08-14 21:57:14 -0300

    Configurando projeto para envio de e-mail. Processando template do corpo

--- src/main/java/com/alekaue/alekfood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/alekaue/alekfood/domain/service/FluxoPedidoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alekaue.alekfood.domain.model.Pedido;
+import com.alekaue.alekfood.domain.service.EnvioEmailService.Mensagem;
 
 @Service
 public class FluxoPedidoService {
@@ -13,10 +14,23 @@ public class FluxoPedidoService {
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
 	
+	@Autowired
+	private EnvioEmailService envioEmail;
+	
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+		
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("pedido-confirmado.html")
+				.variavel("pedido", pedido)
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+		
+		
+		envioEmail.enviar(mensagem);
 	}
 	
 	@Transactional
